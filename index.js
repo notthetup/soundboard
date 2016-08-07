614b15d48594cd660b06bab483b5f0bf3354b720
Chinmay Pendharkar <notthetup@gmail.com>
2016-08-07 15:51:29 +0800

    added config

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var mumble = require('mumble');
 var fs = require('fs');
 var wav = require('wav');
 var Mixer = require('audio-mixer');
+var midi = require('midi');
+
+var config = require('./config.json');
+
+var NOTEON = 128
+var NOTEOFF = 144;
 
 var format = {
   endianness: 'LE',
@@ -12,42 +18,62 @@ var format = {
   bitDepth: 16
 };
 
-
-var reader = new wav.Reader();
+var currentlyPlaying = [];
 
 var mixer = new Mixer({
-	channels: format.channels
+  channels: format.channels
 });
 
-var mixInput, connection;
+// Set up a new input.
+var portNum;
+var input = new midi.input();
+var numPorts = input.getPortCount();
+for (var index=0; index< numPorts; index++){
+  if (input.getPortName(index) === config.portName){
+    portNum = index;
+    break;
+  }
+}
 
-var options = {
-    key: fs.readFileSync( 'key.pem' ),
-    cert: fs.readFileSync( 'cert.pem' )
-};
+console.log("Opening MIDI Port...");
+input.openPort(portNum);
 
-console.log( 'Connecting' );
-mumble.connect( 'mumble://listen.webuild.sg', options, function ( error, conn ) {
-    if( error ) { throw new Error( error ); }
+connectToMumble(config.url, "FX", {
+    key: fs.readFileSync( config.auth.key ),
+    cert: fs.readFileSync( config.auth.cert )
+  }, onConnection);
 
-    console.log( 'Connected' );
-    connection = conn;
 
-    connection.authenticate( 'ExampleUser');
-    connection.on( 'initialized', function(){
-      // the "format" event gets emitted at the end of the WAVE header
-
-      mixer.pipe(connection.inputStream(format));
-
-      playFile('rimshot_mono.wav');
-      setTimeout(function(){
-        playFile('rimshot_mono.wav');
-      },10000);
-
+function onConnection(outputStream){
+  mixer.pipe(outputStream);
+  console.log("Registering MIDI Handler");
+  input.on('message', function(deltaTime, message) {
+    var messageType = message[0];
+    var channel = message[1];
+    var velocity = message[2];
+    config.sounds.forEach((thisSound) => {
+      if (channel !== thisSound.channel) return;
+      if (messageType === NOTEON && thisSound.mode === "trigger"){
+        currentlyPlaying.push(playFile(thisSound.file, thisSound.gain));
+      }
     });
-});
+  });
+}
 
-function playFile(filename){
+function connectToMumble(url, username, options, callback){
+  console.log( 'Connecting...' );
+  mumble.connect( url, options, function ( error, conn ) {
+      if( error ) { throw new Error( error ); }
+      console.log( 'Connected to', url );
+      conn.authenticate( username);
+      conn.on( 'initialized', function(){
+        callback(conn.inputStream(format));
+      });
+  });
+}
+
+function playFile(filename, gain){
+  console.log("Playing...", filename);
   var reader = new wav.Reader();
   reader.pipe(mixer.input({
     sampleRate: format.sampleRate,
@@ -55,4 +81,8 @@ function playFile(filename){
     bitDepth: format.bitDepth
   }));
   fs.createReadStream(filename).pipe(reader);
+  reader.on('finish', () =>{
+    currentlyPlaying.splice(currentlyPlaying.indexOf(reader),1);
+  });
+  return reader;
 }
